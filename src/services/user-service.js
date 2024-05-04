82fe8b29d22557ccfb0af308bc33cc506c713f03
kuldeep Raj Gour <kuldeepgour002@gmail.com>
2024-05-04 16:08:08 +0530

    check password function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository = require('../repository/user-repository');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const { JWT_KEY } = require('../config/serverConfig');
 
 class UserService {
@@ -40,6 +41,14 @@ class UserService {
         }
     }
 
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password comparison");
+            throw error;
+        }
+    }
 }
 
 module.exports  =UserService;
