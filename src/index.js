5bf19753543b4c05f8ac95b0d334ceb84afd3238
kuldeep Raj Gour <kuldeepgour002@gmail.com>
2024-05-04 16:04:12 +0530

    to verify and create a token

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+// const UserService = require('./services/user-service');
+
 const db = require('./models/index');
 
 const app = express();
